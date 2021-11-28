22f29c83aae5f28b7b3e19576505047046e5e734
KRISHNA TEJA <krishnateja72692@hotmail.com>
2021-11-29 02:05:10 +0530

    imgur test fix

--- clientapp/src/components/social/InstagramAuth.tsx
+++ clientapp/src/components/social/InstagramAuth.tsx
@@ -11,10 +11,6 @@ export const InstagramAuth = () => {
   const [isSharingPost, setIsSharingPost] = useState(false);
   const [fbUserAccessToken, setFbUserAccessToken] = useState("");
 
-  const changeImageUrl = (link:any) => {
-    setImageUrl(link);
-  }
-
   const handleFile = (e:any) => {
    const data = new FormData();
    data.append('image', e.target.files[0]);
@@ -31,7 +27,7 @@ export const InstagramAuth = () => {
   .then(function (response) {
     // If response is success get image url and assign it to setImageUrl
     if(response.data.success === true) {
-      changeImageUrl(response.data.link);
+      setImageUrl(response.data['link']);
     }
   })
   .catch(function (error) {
